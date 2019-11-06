4553ca1f24787c8ab094dccc5154553ac661b87a
Rick Lan <rick.lan@simpro.co>
2019-11-06 15:13:52 +1000

    add zh_CN locale

--- offroad/js/utils/I18n.js
+++ offroad/js/utils/I18n.js
@@ -2,11 +2,13 @@ import { NativeModules } from 'react-native'
 import { setupI18n } from "@lingui/core"
 import en from '../locales/en/messages.js';
 import zhTW from '../locales/zh_TW/messages.js';
+import zhCN from '../locales/zh_CN/messages.js';
 
 // define which locale use which translate file
 let supportedLanguage = {
     'en': en,
     'zh_TW': zhTW,
+    'zh_CN': zhCN,
 };
 
 const locale = NativeModules.I18nManager.localeIdentifier; // zh_TW_#Hant, zh_CN_#Hans
