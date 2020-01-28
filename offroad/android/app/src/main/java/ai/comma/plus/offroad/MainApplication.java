f32176ecff1e12d317209f6e858c95986cf3ed0f
xx979xx <xx979xx@github.com>
2020-01-28 22:32:11 +0300

    support Right to Left

--- offroad/android/app/src/main/java/ai/comma/plus/offroad/MainApplication.java
+++ offroad/android/app/src/main/java/ai/comma/plus/offroad/MainApplication.java
@@ -14,6 +14,7 @@ import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.react.modules.i18nmanager.I18nUtil;
 
 import java.util.Arrays;
 import java.util.List;
@@ -60,5 +61,9 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
 
     SoLoader.init(this, /* native exopackage */ false);
+
+    I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
+    sharedI18nUtilInstance.allowRTL(getApplicationContext(), true);
+
   }
 }
