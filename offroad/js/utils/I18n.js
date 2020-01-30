0bbbf4888f5bb597061c0804e6690ba7f0c9eede
xx979xx <xx979xx@github.com>
2020-01-30 18:53:14 +0300

    translate the data

--- offroad/js/utils/I18n.js
+++ offroad/js/utils/I18n.js
@@ -4,6 +4,8 @@ import en from '../locales/en/messages.js';
 import zhTW from '../locales/zh_TW/messages.js';
 import zhCN from '../locales/zh_CN/messages.js';
 import ar from '../locales/ar/messages.js';
+import moment from 'moment';
+import 'moment/locale/ar';
 
 // define which locale use which translate file
 let supportedLanguage = {
@@ -25,5 +27,5 @@ if (supportedLanguage.hasOwnProperty(locale)) {
 } else {
     i18n.activate('en');
 }
-
+moment.locale('ar')
 export default i18n
