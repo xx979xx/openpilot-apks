6388cb613f99feb426153bcd9d0c3c60c4a4a52f
xx979xx <40252818+xx979xx@users.noreply.github.com>
2020-01-31 11:25:19 +0300

    date translation

--- offroad/js/utils/I18n.js
+++ offroad/js/utils/I18n.js
@@ -4,13 +4,16 @@ import en from '../locales/en/messages.js';
 import zhTW from '../locales/zh_TW/messages.js';
 import zhCN from '../locales/zh_CN/messages.js';
 import koKR from '../locales/ko_KR/messages.js';
+import moment from 'moment';
+import 'moment/locale/zhTW';
+import 'moment/locale/zhCN';
+import 'moment/locale/ko';
 
 // define which locale use which translate file
 let supportedLanguage = {
-    'en': en,
     'zh_TW': zhTW,
     'zh_CN': zhCN,
-    'ko_KR': koKR,
+    'ko_KR': ko,
 };
 
 const locale = NativeModules.I18nManager.localeIdentifier; // zh_TW_#Hant, zh_CN_#Hans
@@ -20,6 +23,7 @@ i18n.load(supportedLanguage);
 // if the locale is defined, activate it
 if (supportedLanguage.hasOwnProperty(locale)) {
     i18n.activate(locale);
+    moment.locale(supportedLanguage[locale])
 } else {
     i18n.activate('en');
 }
