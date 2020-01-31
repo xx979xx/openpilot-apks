87fe6a4ad3f41087e96d6138c6475f848a9d81c1
xx979xx <xx979xx@github.com>
2020-02-01 00:13:49 +0300

    date translation

--- offroad/js/utils/I18n.js
+++ offroad/js/utils/I18n.js
@@ -1,12 +1,20 @@
 import { NativeModules } from 'react-native'
 import { setupI18n } from "@lingui/core"
+import enUS from '../locales/en_US/messages.js';
 import zhTW from '../locales/zh_TW/messages.js';
 import zhCN from '../locales/zh_CN/messages.js';
 import koKR from '../locales/ko_KR/messages.js';
 import arEG from '../locales/ar_EG/messages.js';
+import moment from 'moment';
+import 'moment/locale/zh-tw';
+import 'moment/locale/zh-cn';
+import 'moment/locale/ko';
+import 'moment/locale/ar';
+
 
 // define which locale use which translate file
 let supportedLanguage = {
+    'en_US': enUS,
     'zh_TW': zhTW,
     'zh_CN': zhCN,
     'ko_KR': koKR,
@@ -20,6 +28,10 @@ i18n.load(supportedLanguage);
 // if the locale is defined, activate it
 if (supportedLanguage.hasOwnProperty(locale)) {
     i18n.activate(locale);
+    moment.locale(locale);
+} else {
+    i18n.activate('en_US');
+    moment.locale('en_US');
 }
 
 export default i18n
