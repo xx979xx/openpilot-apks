42368827ac26a5dc5918dd47c01dd0cd33d13efd
xx979xx <xx979xx@github.com>
2020-01-31 22:08:04 +0300

    add Arabic locale

--- offroad/js/utils/I18n.js
+++ offroad/js/utils/I18n.js
@@ -3,12 +3,14 @@ import { setupI18n } from "@lingui/core"
 import zhTW from '../locales/zh_TW/messages.js';
 import zhCN from '../locales/zh_CN/messages.js';
 import koKR from '../locales/ko_KR/messages.js';
+import arEG from '../locales/ar_EG/messages.js';
 
 // define which locale use which translate file
 let supportedLanguage = {
     'zh_TW': zhTW,
     'zh_CN': zhCN,
     'ko_KR': koKR,
+    'ar_EG': arEG,
 };
 
 const locale = NativeModules.I18nManager.localeIdentifier; // zh_TW_#Hant, zh_CN_#Hans
