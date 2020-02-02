ef4d1df4e344b229a6aab776d49c4841d56f1f98
xx979xx <40252818+xx979xx@users.noreply.github.com>
2020-02-03 01:38:48 +0300

    style

--- offroad/js/locales/ar_EG/extra.js
+++ offroad/js/locales/ar_EG/extra.js
@@ -1,4 +1,5 @@
 module.exports={
+    cityNames:{"Yanbu":"ينبع","Turaif":"طريف‎‎","At Tūbī":"At Tūbī","Tārūt":"Tārūt","Tārūt":"تاروت","Tabuk":"تبوك","Ţubarjal":"طبرجل","Sayhāt":"سيهات","Sakakah":"سكاكا","Şafwá":"صفوى","Şabyā":"صبيا","Rābigh":"Rābigh","Najrān":"نجران","Mislīyah":"مسلية","Marāt":"مرات","Mecca":"مكة المكرمة","Khamis Mushait":"خميس مشيط","Jizan":"جازان","Jeddah":"جدة","Ha'il":"حائل","Hafar Al-Batin":"حفر الباطن‎","Duba":"ضبا","Buraydah":"بُرَيدَة","Abqaiq":"بقيق","Az Zulfi":"الزُّلفِي","Dhahran":"الظهران","Aţ Ţaraf":"الطرف","Ta’if":"الطائف","Sājir":"ساجر","Riyadh":"الرياض","Ar Rass":"الرس","Ar Rass":"الرَّس","Arar":"عرعر","An Nimāş":"An Nimāş","Qurayyat":"القريات","Al-`Ula":"العلا","Qaisumah":"القيصومة‎‎","Al Qaţīf":"القطيف","Al Muţayrifī":"المطيرفي","Al Mubarraz":"المبرز","Al Mithnab":"المذنب","Medina":"المدينة","Medina":"المدينة المنورة","Khobar":"محافظة الخبر","Al Kharj":"الخرج","Al Jumūm":"الجموم","Al Jubayl":"الجبيل","Al Jafr":"الجفر","Al Hufūf":"الهفوف","Al Bukayrīyah":"البُكَيرِيَّة","Al Bahah":"الباحة","Al Arţāwīyah":"الارطاوية","Al Awjām":"الاوجام","‘Afīf":"‘Afīf","Ad Dilam":"الدلم","Ad Darb":"الدرب","Dammam":"الدمام","Abū ‘Arīsh":"ٲبو عريش","Abha":"أبها","Tumayr":"تمير","Al Fuwayliq":"القويلق","Al Hadā":"الهدا","Ash Shafā":"الشفا","Mizhirah":"مزهرة"},
     numberMap:{
         '1': '١',
         '2': '٢',
@@ -11,5 +12,5 @@ module.exports={
         '9': '٩',
         '0': '٠',
         ',': ',',
-    },
-    cityNames:{"Yanbu":"ينبع","Turaif":"طريف‎‎","At Tūbī":"At Tūbī","Tārūt":"Tārūt","Tārūt":"تاروت","Tabuk":"تبوك","Ţubarjal":"طبرجل","Sayhāt":"سيهات","Sakakah":"سكاكا","Şafwá":"صفوى","Şabyā":"صبيا","Rābigh":"Rābigh","Najrān":"نجران","Mislīyah":"مسلية","Marāt":"مرات","Mecca":"مكة المكرمة","Khamis Mushait":"خميس مشيط","Jizan":"جازان","Jeddah":"جدة","Ha'il":"حائل","Hafar Al-Batin":"حفر الباطن‎","Duba":"ضبا","Buraydah":"بُرَيدَة","Abqaiq":"بقيق","Az Zulfi":"الزُّلفِي","Dhahran":"الظهران","Aţ Ţaraf":"الطرف","Ta’if":"الطائف","Sājir":"ساجر","Riyadh":"الرياض","Ar Rass":"الرس","Ar Rass":"الرَّس","Arar":"عرعر","An Nimāş":"An Nimāş","Qurayyat":"القريات","Al-`Ula":"العلا","Qaisumah":"القيصومة‎‎","Al Qaţīf":"القطيف","Al Muţayrifī":"المطيرفي","Al Mubarraz":"المبرز","Al Mithnab":"المذنب","Medina":"المدينة","Medina":"المدينة المنورة","Khobar":"محافظة الخبر","Al Kharj":"الخرج","Al Jumūm":"الجموم","Al Jubayl":"الجبيل","Al Jafr":"الجفر","Al Hufūf":"الهفوف","Al Bukayrīyah":"البُكَيرِيَّة","Al Bahah":"الباحة","Al Arţāwīyah":"الارطاوية","Al Awjām":"الاوجام","‘Afīf":"‘Afīf","Ad Dilam":"الدلم","Ad Darb":"الدرب","Dammam":"الدمام","Abū ‘Arīsh":"ٲبو عريش","Abha":"أبها","Tumayr":"تمير","Al Fuwayliq":"القويلق","Al Hadā":"الهدا","Ash Shafā":"الشفا","Mizhirah":"مزهرة"}}
+    }
+}
